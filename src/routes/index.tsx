9f888654e340181908b7ebc015874bb355373eea
FrostyyPaws <thefrostyypaws@gmail.com>
2023-05-27 19:37:39 -0400

    broken functionality but HUUUUGE type update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,128 +1,135 @@
-import { component$, useSignal, useStylesScoped$, useVisibleTask$ } from "@builder.io/qwik";
-import { type DocumentHead, server$ } from "@builder.io/qwik-city";
+import {
+  component$,
+  useSignal,
+  useStylesScoped$,
+  useVisibleTask$,
+} from "@builder.io/qwik";
+import { server$, type DocumentHead } from "@builder.io/qwik-city";
 
 import Counter from "~/components/starter/counter/counter";
 import Hero from "~/components/starter/hero/hero";
 import Infobox from "~/components/starter/infobox/infobox";
 import Starter from "~/components/starter/next-steps/next-steps";
-import { livingData } from "./living-data";
-
-export const useLivingData = livingData({
-    qrl: server$(async function (setMessage?: string, setNumber?: number, ) {
-        const rand = Math.random();
-        return `${setMessage ?? "Juicy data!"} ${setNumber ?? rand}`;
-    }),
-    startingValue: "Loading....",
-});
+import { HasMandatoryParameters, livingData } from "./living-data";
 
 export default component$(() => {
+  const show = useSignal(false);
+  useVisibleTask$(() => {
+    setTimeout(() => {
+      show.value = false;
+    }, 2000);
+  });
+  return (
+    <>
+      <JuicyData />
+      {show.value && <JuicyData />}
+      <Hero />
+      <Starter />
 
-    const show = useSignal(false);
-    useVisibleTask$(()=> { 
-        setTimeout(()=> { 
-            show.value = false;
-        }, 2000)
-    })
-    return (
-        <>
-            <JuicyData />
-           {show.value &&  <JuicyData />}
-            <Hero />
-            <Starter />
+      <div role="presentation" class="ellipsis"></div>
+      <div role="presentation" class="ellipsis ellipsis-purple"></div>
 
-            <div role="presentation" class="ellipsis"></div>
-            <div role="presentation" class="ellipsis ellipsis-purple"></div>
+      <div class="container container-center container-spacing-xl">
+        <h3>
+          You can <span class="highlight">count</span>
+          <br /> on me
+        </h3>
+        <Counter />
+      </div>
 
-            <div class="container container-center container-spacing-xl">
-                <h3>
-                    You can <span class="highlight">count</span>
-                    <br /> on me
-                </h3>
-                <Counter />
+      <div class="container container-flex">
+        <Infobox>
+          <div q:slot="title" class="icon icon-cli">
+            CLI Commands
+          </div>
+          <>
+            <p>
+              <code>npm run dev</code>
+              <br />
+              Starts the development server and watches for changes
+            </p>
+            <p>
+              <code>npm run preview</code>
+              <br />
+              Creates production build and starts a server to preview it
+            </p>
+            <p>
+              <code>npm run build</code>
+              <br />
+              Creates production build
+            </p>
+            <p>
+              <code>npm run qwik add</code>
+              <br />
+              Runs the qwik CLI to add integrations
+            </p>
+          </>
+        </Infobox>
+
+        <div>
+          <Infobox>
+            <div q:slot="title" class="icon icon-apps">
+              Example Apps
             </div>
+            <p>
+              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
+              <a href="/demo/todolist">Todo App</a>.
+            </p>
+          </Infobox>
 
-            <div class="container container-flex">
-                <Infobox>
-                    <div q:slot="title" class="icon icon-cli">
-                        CLI Commands
-                    </div>
-                    <>
-                        <p>
-                            <code>npm run dev</code>
-                            <br />
-                            Starts the development server and watches for changes
-                        </p>
-                        <p>
-                            <code>npm run preview</code>
-                            <br />
-                            Creates production build and starts a server to preview it
-                        </p>
-                        <p>
-                            <code>npm run build</code>
-                            <br />
-                            Creates production build
-                        </p>
-                        <p>
-                            <code>npm run qwik add</code>
-                            <br />
-                            Runs the qwik CLI to add integrations
-                        </p>
-                    </>
-                </Infobox>
-
-                <div>
-                    <Infobox>
-                        <div q:slot="title" class="icon icon-apps">
-                            Example Apps
-                        </div>
-                        <p>
-                            Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-                            <a href="/demo/todolist">Todo App</a>.
-                        </p>
-                    </Infobox>
-
-                    <Infobox>
-                        <div q:slot="title" class="icon icon-community">
-                            Community
-                        </div>
-                        <ul>
-                            <li>
-                                <span>Questions or just want to say hi? </span>
-                                <a href="https://qwik.builder.io/chat" target="_blank">
-                                    Chat on discord!
-                                </a>
-                            </li>
-                            <li>
-                                <span>Follow </span>
-                                <a href="https://twitter.com/QwikDev" target="_blank">
-                                    @QwikDev
-                                </a>
-                                <span> on Twitter</span>
-                            </li>
-                            <li>
-                                <span>Open issues and contribute on </span>
-                                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                                    GitHub
-                                </a>
-                            </li>
-                            <li>
-                                <span>Watch </span>
-                                <a href="https://qwik.builder.io/media/" target="_blank">
-                                    Presentations, Podcasts, Videos, etc.
-                                </a>
-                            </li>
-                        </ul>
-                    </Infobox>
-                </div>
+          <Infobox>
+            <div q:slot="title" class="icon icon-community">
+              Community
             </div>
-        </>
-    );
+            <ul>
+              <li>
+                <span>Questions or just want to say hi? </span>
+                <a href="https://qwik.builder.io/chat" target="_blank">
+                  Chat on discord!
+                </a>
+              </li>
+              <li>
+                <span>Follow </span>
+                <a href="https://twitter.com/QwikDev" target="_blank">
+                  @QwikDev
+                </a>
+                <span> on Twitter</span>
+              </li>
+              <li>
+                <span>Open issues and contribute on </span>
+                <a href="https://github.com/BuilderIO/qwik" target="_blank">
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <span>Watch </span>
+                <a href="https://qwik.builder.io/media/" target="_blank">
+                  Presentations, Podcasts, Videos, etc.
+                </a>
+              </li>
+            </ul>
+          </Infobox>
+        </div>
+      </div>
+    </>
+  );
 });
 
-export const JuicyData = component$(() => {
-    const data = useLivingData("Initial!:");
+export const useLivingData = livingData(
+  server$(async function (setMessage?: string, setNumber?: number) {
+    const rand = Math.random();
+    return `${setMessage ?? "Juicy data!"} ${setNumber ?? rand}`;
+  })
+);
 
-    useStylesScoped$(`
+
+
+export const JuicyData = component$(() => {
+  const data = useLivingData({initialArgs: ["heyyy"], startingValue: "heyyyy"});
+
+  data.signal.value;
+
+  useStylesScoped$(`
         section { 
             margin: auto;
             width: 100%;
@@ -132,34 +139,42 @@ export const JuicyData = component$(() => {
             gap: 10px;
             font-size: 22px;
         }
-    `)
-    return <section>
-        {data.signal.value}
-        <button
-            onClick$={() => {
-                console.log("clicked");
-                data.pause();
-            }}
-        >
-            pause
-        </button>
-        <button onClick$={() => {
-            data.refresh();
-        }}>Pure Refresh</button>
-        <button onClick$={() => {
-            data.newArguments("Refreshed:");
-        }}>New Args</button>
+    `);
+  return (
+    <section>
+      {data.signal.value}
+      <button
+        onClick$={() => {
+          console.log("clicked");
+          data.pause();
+        }}
+      >
+        pause
+      </button>
+      <button
+        onClick$={() => {
+          data.refresh();
+        }}
+      >
+        Pure Refresh
+      </button>
+      <button
+        onClick$={() => {
+          //   data.newArguments("Refreshed:");
+        }}
+      >
+        New Args
+      </button>
     </section>
-})
-
-
+  );
+});
 
 export const head: DocumentHead = {
-    title: "Welcome to Qwik",
-    meta: [
-        {
-            name: "description",
-            content: "Qwik site description",
-        },
-    ],
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
 };
--- src/routes/living-data.ts
+++ src/routes/living-data.ts
@@ -1,137 +1,286 @@
 import {
-    $,
-    useSignal,
-    useVisibleTask$,
-    type QRL,
-    type Signal,
+  $,
+  useSignal,
+  useVisibleTask$,
+  type QRL,
+  type Signal,
 } from "@builder.io/qwik";
 import { server$ } from "@builder.io/qwik-city";
 
 const targetQrlById = new Map<string, QRL>();
 const disconnectRequestsByConnectionId = new Set<number>();
 
-export const livingData = <Q extends QRL>(options: {
-    qrl: Q;
-    interval?: number;
-    startingValue?: Awaited<ReturnType<Q>>;
-}) => {
-    const qrlId = options.qrl.getSymbol();
-    targetQrlById.set(qrlId, options.qrl);
+/*
+
+  // type ReturnValue = {
+  // signal: Signal<undefined | Awaited<ReturnType<Q>>>;
+  // pause: QRL<() => void>;
+  // refresh: QRL<() => void>;
+  // newArguments: QRL<(...args: Parameters<Q>) => void>
+  // }
+  //If we do ReturnValue it makes the consumer swallow the types until they break it down. not very ergo for DX
+  //So we have to repeat here unfortunately
+  type UseLivingData = Parameters<UserFunction> extends []
+    ? () => {
+        signal: Signal<undefined | Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      }
+    : (...args: Parameters<UserFunction>) => {
+        signal: Signal<undefined | Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
 
 
-    // type ReturnValue = {
-        // signal: Signal<undefined | Awaited<ReturnType<Q>>>;
-        // pause: QRL<() => void>;
-        // refresh: QRL<() => void>;
-        // newArguments: QRL<(...args: Parameters<Q>) => void>
-    // }
-    //If we do ReturnValue it makes the consumer swallow the types until they break it down. not very ergo for DX
-    //So we have to repeat here unfortunately
-    type UseLivingData = Parameters<Q> extends []
-        ? () => {
-            signal: Signal<undefined | Awaited<ReturnType<Q>>>;
-            pause: QRL<() => void>;
-            refresh: QRL<() => void>;
-            newArguments: QRL<(...args: Parameters<Q>) => void>
+
+
+        type _UseLivingData = (
+    options: Parameters<UserFunction> extends []
+      ? {
+          interval?: number;
+          startingValue?: StartingValue;
         }
-        : (...args: Parameters<Q>) => {
-            signal: Signal<undefined | Awaited<ReturnType<Q>>>;
-            pause: QRL<() => void>;
-            refresh: QRL<() => void>;
-            newArguments: QRL<(...args: Parameters<Q>) => void>
-        };
+      : {
+          initialArgs: Parameters<UserFunction>;
+          interval?: number;
+          startingValue?: StartingValue;
+        }
+  ) => {
+    signal: StartingValue extends undefined
+      ? Signal<undefined | Awaited<ReturnType<UserFunction>>>
+      : Signal<Awaited<ReturnType<UserFunction>>>;
+    pause: QRL<() => void>;
+    refresh: QRL<() => void>;
+    newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+  };
+*/
 
-    const useLivingData: UseLivingData = function (...args: Parameters<Q>) {
-        const dataSignal = useSignal<undefined | Awaited<ReturnType<Q>>>(options.startingValue);
-        const currentArgs = useSignal<Parameters<Q>>(args);
-        const currentConnection = useSignal<number>(-1);
-        const connections = useSignal<number[]>([]);
+export type HasMandatoryParameters<T extends (...args: any[]) => any> =
+  // distribute over union types
+  Parameters<T> extends [infer P, ...infer Rest]
+    ? // check if the first parameter includes `undefined`
+      P extends undefined
+      ? // if it does, continue checking the rest
+        HasMandatoryParameters<(...args: Rest) => any>
+      : // if it doesn't, the function has mandatory parameters
+        true
+    : // if there are no parameters, the function doesn't have mandatory parameters
+      false;
 
-        type ConnectAndListen = Parameters<Q> extends []
-            ? () => Promise<void>
-            : (...args: Parameters<Q>) => Promise<void>;
+export const livingData = <UserFunction extends QRL>(
+  func: UserFunction
+): Parameters<UserFunction> extends []
+  ? {
+      (): {
+        signal: Signal<undefined | Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
+      (options: {
+        startingValue: Awaited<ReturnType<UserFunction>>;
+        interval?: number;
+      }): {
+        signal: Signal<Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
+      (options: {
+        interval?: number;
+        startingValue?: Awaited<ReturnType<UserFunction>>;
+      }): {
+        signal: Signal<undefined | Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
+    }
+  : HasMandatoryParameters<UserFunction> extends false
+  ? {
+      (): {
+        signal: Signal<undefined | Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
+      (options: {
+        initialArgs?: Parameters<UserFunction>;
+        startingValue: Awaited<ReturnType<UserFunction>>;
+        interval?: number;
+      }): {
+        signal: Signal<Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
+      (options: {
+        initialArgs?: Parameters<UserFunction>;
+        interval?: number;
+        startingValue?: Awaited<ReturnType<UserFunction>>;
+      }): {
+        signal: Signal<undefined | Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
+    }
+  : {
+      (options: {
+        initialArgs: Parameters<UserFunction>;
+        startingValue: Awaited<ReturnType<UserFunction>>;
+        interval?: number;
+      }): {
+        signal: Signal<Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
+      (options: {
+        initialArgs: Parameters<UserFunction>;
+        interval?: number;
+        startingValue?: Awaited<ReturnType<UserFunction>>;
+      }): {
+        signal: Signal<undefined | Awaited<ReturnType<UserFunction>>>;
+        pause: QRL<() => void>;
+        refresh: QRL<() => void>;
+        newArguments: QRL<(...args: Parameters<UserFunction>) => void>;
+      };
+    } => {
+  const qrlId = func.getSymbol();
+  targetQrlById.set(qrlId, func);
 
-        const connectAndListen: ConnectAndListen = $(async () => {
-            const thisConnectionId = Math.random();
-            currentConnection.value = thisConnectionId;
-            await disconnectConnectionInstances(connections.value);
-            connections.value = [...connections.value, thisConnectionId];
-            console.log(connections.value);
-            const stream = await dataFeeder({ qrlId, connectionId: thisConnectionId, args: currentArgs.value });
+  function useLivingData(options?: any) {
+    const dataSignal = useSignal<Awaited<ReturnType<UserFunction>>>(
+      options.startingValue
+    );
 
-            while (true) {
-                const current = await stream.next();
-                if (current.done === true || currentConnection.value !== thisConnectionId) {
-                    break;
-                }
-                dataSignal.value = current.value as Awaited<ReturnType<Q>>;
-            }
-            connections.value = connections.value.filter((id) => id !== thisConnectionId);
-        });
+    const initialArgs =
+      "initialArgs" in options ? options.initialArgs : undefined;
+    const currentArgs = useSignal<Parameters<UserFunction>>(...initialArgs);
+    const currentConnection = useSignal<number>(-1);
+    const connections = useSignal<number[]>([]);
 
+    type ConnectAndListen = Parameters<UserFunction> extends []
+      ? () => Promise<void>
+      : (...args: Parameters<UserFunction>) => Promise<void>;
 
-        const pause = $(async () => {
-            await disconnectConnectionInstances(connections.value);
-        });
+    const connectAndListen: ConnectAndListen = $(async () => {
+      const thisConnectionId = Math.random();
+      currentConnection.value = thisConnectionId;
+      await disconnectConnectionInstances(connections.value);
+      connections.value = [...connections.value, thisConnectionId];
+      console.log(connections.value);
+      const stream = await dataFeeder({
+        qrlId,
+        connectionId: thisConnectionId,
+        args: currentArgs.value,
+      });
 
-        const refresh = $(async () => {
-            await connectAndListen();
-        });
+      while (true) {
+        const current = await stream.next();
+        if (
+          current.done === true ||
+          currentConnection.value !== thisConnectionId
+        ) {
+          break;
+        }
+        dataSignal.value = current.value as Awaited<ReturnType<UserFunction>>;
+      }
+      connections.value = connections.value.filter(
+        (id) => id !== thisConnectionId
+      );
+    });
 
-        const newArguments = $(async (...args: Parameters<Q>) => {
-            currentArgs.value = args;
-            await connectAndListen();
-        });
+    const pause = $(async () => {
+      await disconnectConnectionInstances(connections.value);
+    });
 
-        useVisibleTask$(({ cleanup }) => {
-            cleanup(() => pause());
-            async function stayConnected() {
-                try {
-                    await connectAndListen();
-                } catch (e) {
-                    console.warn("Living data connection lost:", e);
-                    console.log("Retrying");
-                    setTimeout(stayConnected, 500);
-                }
-            }
-            stayConnected();
-        });
+    const refresh = $(async () => {
+      //You'll see the following stayConnected function get repeated.
+      //It's just a way to be able to call itself in the failure case.
+      //A wrapper for staying connected but without really providing any major new logic.
+      //If we try to declare it once out side this and pass it in,
+      //We need to wrap it in $(), which in this case causes a whole host of other issues.
+      //Simpler to just repeat it a few times where necessary.
+      async function stayConnected() {
+        try {
+          await connectAndListen();
+        } catch (e) {
+          console.warn("Living data connection lost:", e);
+          console.log("Retrying");
+          setTimeout(stayConnected, 500);
+        }
+      }
+      stayConnected();
+    });
 
-        return { signal: dataSignal, pause, refresh, newArguments };
-    };
+    const newArguments = $(async (...args: Parameters<UserFunction>) => {
+      currentArgs.value = args;
+      async function stayConnected() {
+        try {
+          await connectAndListen();
+        } catch (e) {
+          console.warn("Living data connection lost:", e);
+          console.log("Retrying");
+          setTimeout(stayConnected, 500);
+        }
+      }
+      stayConnected();
+    });
 
-    return useLivingData;
+    useVisibleTask$(({ cleanup }) => {
+      cleanup(() => pause());
+      async function stayConnected() {
+        try {
+          await connectAndListen();
+        } catch (e) {
+          console.warn("Living data connection lost:", e);
+          console.log("Retrying");
+          setTimeout(stayConnected, 500);
+        }
+      }
+      stayConnected();
+    });
+
+    return { signal: dataSignal, pause, refresh, newArguments };
+  }
+
+  return useLivingData;
 };
 
-export const disconnectConnectionInstances = server$((connectionIds: number[]) => {
+export const disconnectConnectionInstances = server$(
+  (connectionIds: number[]) => {
     connectionIds.forEach((connectionId) => {
-        disconnectRequestsByConnectionId.add(connectionId);
+      disconnectRequestsByConnectionId.add(connectionId);
     });
-});
+  }
+);
 
 export const dataFeeder = server$(async function* (options: {
-    qrlId: string;
-    args: any[];
-    connectionId: number;
-    interval?: number;
+  qrlId: string;
+  args: any[];
+  connectionId: number;
+  interval?: number;
 }) {
-    const func = targetQrlById.get(options.qrlId)!;
+  const func = targetQrlById.get(options.qrlId)!;
 
-    yield await func(...options.args);
+  yield await func(...options.args);
 
-    let lastCompleted = Date.now();
-    const interval = options?.interval || 2500;
-    while (
-        disconnectRequestsByConnectionId.has(options.connectionId) === false
-    ) {
-        if (Date.now() - lastCompleted >= interval) {
-            yield await func(...options.args);
-            lastCompleted = Date.now();
-        }
-        await pause(40);
+  let lastCompleted = Date.now();
+  const interval = options?.interval || 2500;
+  while (disconnectRequestsByConnectionId.has(options.connectionId) === false) {
+    if (Date.now() - lastCompleted >= interval) {
+      yield await func(...options.args);
+      lastCompleted = Date.now();
     }
+    await pause(40);
+  }
 });
 
 export function pause(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
