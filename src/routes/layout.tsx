d519e61c4c1ed50b4cbe1a99c8c4fa663d32eaa9
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-06-01 20:28:42 -0400

    keep it more reliable

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,5 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { type RequestHandler, routeLoader$ } from '@builder.io/qwik-city';
 
 import Footer from '~/components/starter/footer/footer';
 import Header from '~/components/starter/header/header';
@@ -14,6 +14,20 @@ export const useServerTimeLoader = routeLoader$(() => {
     date: new Date().toISOString(),
   };
 });
+
+export const onRequest: RequestHandler = async (event) => {
+  const dbHost = event.env.get("DATABASE_HOST");
+  const dbUsername = event.env.get("DATABASE_USERNAME");
+  const dbPassword = event.env.get("DATABASE_PASSWORD");
+  if (!dbHost || !dbUsername || !dbPassword) {
+    throw new Error("Missing database credentials");
+  }
+  initializeDb({ 
+    host: dbHost,
+    username: dbUsername,
+    password: dbPassword,
+  });
+};
 
 export const useDbSetupAndGetUsername =  routeLoader$(async (event) => { 
   const dbHost = event.env.get("DATABASE_HOST");
