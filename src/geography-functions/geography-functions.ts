bca38127d0a6d9f3cfe1b3205e3d0f2c8fcc651a
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-06-05 08:56:27 -0400

    transit

--- src/geography-functions/geography-functions.ts
+++ src/geography-functions/geography-functions.ts
@@ -1,21 +1,16 @@
 export function findBoundingBox(coords: {lat: number, lon: number}[]) { 
-    const latitudes = [];
-    const longitudes = [];
-    for (let i = 0; i < coords.length; i++) {
-        latitudes.push(coords[i].lat);
-        longitudes.push(coords[i].lon);
-    }
-
-    let minLat = latitudes[0];
-    let maxLat = latitudes[0];
-    let minLon = longitudes[0];
-    let maxLon = longitudes[0];
     
-    for (let i = 1; i < latitudes.length; i++) {
-        minLat = Math.min(minLat, latitudes[i]);
-        maxLat = Math.max(maxLat, latitudes[i]);
-        minLon = Math.min(minLon, longitudes[i]);
-        maxLon = Math.max(maxLon, longitudes[i]);
+    let minLat = coords[0].lat;
+    let maxLat = coords[0].lat;
+    let minLon =  coords[0].lon;
+    let maxLon =  coords[0].lon;
+
+    for (let i = 0; i < coords.length; i++) {
+
+        minLat = Math.min(minLat, coords[i].lat);
+        maxLat = Math.max(maxLat, coords[i].lat);
+        minLon = Math.min(minLon, coords[i].lon);
+        maxLon = Math.max(maxLon, coords[i].lon);
     }
     
     return {southwest: {lat: minLat, long: minLon}, northeast: {lat: maxLat, long: maxLon}}
---
+++ src/routes/transit/index.tsx
@@ -0,0 +1,141 @@
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { routeLoader$, server$ } from "@builder.io/qwik-city";
+import { XMLParser } from "fast-xml-parser";
+import { livingData } from "~/living-data/living-data";
+import { TRANSIT_AGENCIES } from "./transit-routes";
+import { BusFlippedIcon, BusIcon } from "~/components/icons";
+
+export const getVehiclesInfo = server$(async function () {
+  const vehicles = await getLiveVehiclesInfo("San Francisco Muni Sandbox", "1");
+  return vehicles;
+});
+
+export const useLoadedVehiclesInfo = routeLoader$((event) =>
+  getVehiclesInfo.call(event)
+);
+export const useVehiclesInfo = livingData(getVehiclesInfo);
+
+export const STARTING_ROTATION = -120 as const;
+export default component$(() => {
+  const vehicles = useVehiclesInfo({
+    startingValue: useLoadedVehiclesInfo().value,
+    interval: 5000,
+  });
+
+  useStylesScoped$(` 
+    section { 
+      display: flex;
+      flex-wrap: wrap;
+      gap: 40px;
+    }
+  `);
+  return (
+    <main>
+      <section>
+        {vehicles.signal.value
+          .sort((a, b) => {
+            if (a.speedKmHr === "0") {
+              return 1;
+            }
+            if (b.speedKmHr === "0") {
+              return -1;
+            }
+            return 0;
+          })
+          .map((vehicle) => {
+            const heading = normalizeTo360(-40); //normalizeTo360(Number(vehicle.heading));
+            const basePosition = (STARTING_ROTATION + heading) * -1;
+            console.log({ heading });
+            return (
+              <div class="agency" key={vehicle.id}>
+                {vehicle.id}
+                  <BusIcon
+                    height={50}
+                    width={50}
+                    style={{
+                      transform: `rotate(${basePosition}deg) scaleY(-1)`,
+                    }}
+                  />
+                <br />
+                {vehicle.lat} {vehicle.lon} <br />
+                {degreesToCompass(heading)} {vehicle.heading}
+                <br />
+                {vehicle.speedKmHr === "0"
+                  ? "Stopped"
+                  : `${vehicle.speedKmHr} km/h`}
+              </div>
+            );
+          })}
+      </section>
+    </main>
+  );
+});
+
+export async function getLiveVehiclesInfo<
+  AGENCY extends keyof typeof TRANSIT_AGENCIES
+>(
+  agencyTitle: AGENCY,
+  routeTag: (typeof TRANSIT_AGENCIES)[AGENCY]["routeTags"][number]
+) {
+  const url = new URL(`https://retro.umoiq.com/service/publicXMLFeed`);
+  url.searchParams.set("command", "vehicleLocations");
+  url.searchParams.set("a", TRANSIT_AGENCIES[agencyTitle].tag);
+  url.searchParams.set("r", routeTag);
+  url.searchParams.set("t", "1");
+  const xmlResponse = await fetch(url.toString()).then((res) => res.text());
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+  });
+  const json = parser.parse(xmlResponse) as {
+    "?xml": { "@_version": string; "@_encoding": string };
+    body: {
+      "@_copyright": string;
+      lastTime: { "@_time": `${number}` };
+      Error: {
+        "#text": string;
+        "@_shouldRetry": "false" | "true";
+      };
+      vehicle?: Array<{
+        "@_id": string;
+        "@_lat": `${number}`;
+        "@_lon": `${number}`;
+        "@_secsSinceReport": `${number}`;
+        "@_predictable": "true" | "false";
+        "@_heading": `${number}`;
+        "@_speedKmHr": `${number}`;
+      }>;
+    };
+  };
+  if (!json.body.vehicle) {
+    return [];
+  }
+  return json.body.vehicle.map((vehicle) => ({
+    id: vehicle["@_id"],
+    lat: vehicle["@_lat"],
+    lon: vehicle["@_lon"],
+    secsSinceReport: vehicle["@_secsSinceReport"],
+    predictable: vehicle["@_predictable"],
+    heading: vehicle["@_heading"],
+    speedKmHr: vehicle["@_speedKmHr"],
+  }));
+}
+
+export function degreesToCompass(degrees: number): string {
+  const normalized = normalizeTo360(degrees);
+  const compassPoints = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+  const degreesPerDirection = 360 / compassPoints.length;
+  const shiftAmount = degreesPerDirection / 2;
+  const index =
+    Math.floor((normalized + shiftAmount) / degreesPerDirection) %
+    compassPoints.length;
+  return compassPoints[index];
+}
+
+export function normalizeTo360(degrees: number) {
+  const normalized = degrees % 360;
+  if (normalized < 0) {
+    return normalized + 360;
+  } else {
+    return normalized;
+  }
+}
