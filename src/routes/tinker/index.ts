14cb0fc97e293c2effea02e6abbbd7d8e57f7165
KenParsons <90424167+KenAKAFrosty@users.noreply.github.com>
2023-06-05 14:53:31 -0400

    hmmm

--- src/routes/tinker/index.ts
+++ src/routes/tinker/index.ts
@@ -1,4 +1,4 @@
-import * as Ably from "ably/promises"
+
 import { type RequestHandler } from "@builder.io/qwik-city";
 //https://github.com/ably/ably-js#using-the-realtime-api
 export const onGet: RequestHandler = async (event) => {
@@ -7,16 +7,16 @@ export const onGet: RequestHandler = async (event) => {
     if (!key) {
         throw new Error("ABLY_KEY not set");
     }
-    const client = new Ably.Realtime.Promise(key);
-    client.connection.on("connected", () => { 
-        console.log("connected");
-    });
-    client.connection.on("closed", () => {
-        console.log("disconnected");
-    });
-    client.connection.on("failed", () => {
-        console.log("failed");
-    });
+    // const client = new Ably.Realtime.Promise(key);
+    // client.connection.on("connected", () => { 
+    //     console.log("connected");
+    // });
+    // client.connection.on("closed", () => {
+    //     console.log("disconnected");
+    // });
+    // client.connection.on("failed", () => {
+    //     console.log("failed");
+    // });
 
     const response = await fetch("https://realtime.ably.io/channels/getting-started/messages", {
         method: "GET",
@@ -29,7 +29,7 @@ export const onGet: RequestHandler = async (event) => {
         //     data: "hello there"
         // })
     }).then((res) => res.json());
-    client.connection.close();
+    // client.connection.close();
     console.log(response);
     event.json(200, response);
 }
